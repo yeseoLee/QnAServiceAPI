f77b8fe0bcab2f0319d6e1d69c705edc848bf776
이예서 <49704047+yeseoLee@users.noreply.github.com>
2023-03-07 00:04:55 +0900

    sqlite3 table 생성 쿼리 추가

--- datasource/sqlite3.go
+++ datasource/sqlite3.go
@@ -3,6 +3,7 @@ package datasource
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -33,6 +34,7 @@ func (s sqlite) SqliteConnect() sqlite {
 	}
 	s.dbpool = db
 	s.ok = true
+	s.createTable()
 	return s
 }
 
@@ -41,4 +43,54 @@ func (s sqlite) GetConnection() (*sql.DB, error) {
 		return nil, fmt.Errorf("fail to connect sqlite db")
 	}
 	return s.dbpool, nil
+}
+
+func (s sqlite) createTable() {
+	createQuestionTableQuery := `
+		CREATE TABLE IF NOT EXISTS tbQuestion ( 
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		writerId VARCHAR(255) NOT NULL,
+		title VARCHAR(255) NOT NULL,
+		content TEXT,
+		tags TEXT,
+		images TEXT,
+		isAccept BOOLEAN NOT NULL CHECK (isAccept IN (0, 1)),
+		createdAt DATETIME,
+		updatedAt DATETIME,
+		deletedAt DATETIME
+		);
+	`
+	createAnswerTableQuery := `
+		CREATE TABLE IF NOT EXISTS tbAnswer ( 
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		questionId INTEGER UNIQUE NOT NULL,
+		writerId VARCHAR(255) NOT NULL,
+		content TEXT,
+		images TEXT,
+		isAccept BOOLEAN NOT NULL CHECK (isAccept IN (0, 1)),
+		createdAt DATETIME,
+		updatedAt DATETIME,
+		deletedAt DATETIME
+		);
+	`
+	createCommentTableQuery := `
+		CREATE TABLE IF NOT EXISTS tbComment ( 
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		questionId INTEGER UNIQUE NOT NULL,
+		answerId INTEGER UNIQUE NOT NULL,
+		writerId VARCHAR(255) NOT NULL,
+		content TEXT,
+		createdAt DATETIME,
+		deletedAt DATETIME
+		);
+	`
+	if _, err := s.dbpool.Exec(createQuestionTableQuery); err != nil {
+		log.Fatal(fmt.Errorf("createQuestionTableQuery error: %w", err))
+	}
+	if _, err := s.dbpool.Exec(createAnswerTableQuery); err != nil {
+		log.Fatal(fmt.Errorf("createAnswerTableQuery error: %w", err))
+	}
+	if _, err := s.dbpool.Exec(createCommentTableQuery); err != nil {
+		log.Fatal(fmt.Errorf("createCommentTableQuery error: %w", err))
+	}
 }
